c8c609e668051e38b5e039f5a56c5387f884c2ca
Yash-Garg <ben10.yashgarg@gmail.com>
2020-04-26 20:21:25 +0530

    Add AUTH_USERS and AUTH_CHATS * Use backticks instead of quotes
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
-const TelegramBot = require("node-telegram-bot-api");
+const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config.js')
 const ping = require('./ping/ping.js');
 const axios = require('axios')
 
 const bot = new TelegramBot(config.TOKEN, { polling: true });
 
-var hosts = ['https://api.telegram.org', 'https://google.co.in'];
-const API_URL = "https://wallhaven.cc/api/v1/";
+var hosts = [`https://api.telegram.org', 'https://google.co.in`];
+const API_URL = `https://wallhaven.cc/api/v1/`;
 const API_INDEX = `&apikey=${config.API_WALLHAVEN}`;
 const WALL_URL = `${API_URL}w/`
 
@@ -41,7 +41,7 @@ const get_wall_using_id = async (query) => {
     let response = await axios.get(
         `${WALL_URL}${id}?apikey=${config.API_WALLHAVEN}`
     );
-    let data = (await response.data.data) ? response.data.data.path : "404";
+    let data = (await response.data.data) ? response.data.data.path : `404`;
     return data;
 };
 
@@ -55,83 +55,140 @@ const toplist = async (query) => {
 };
 
 //
-// C O M M A N D S  S T U F F
+//  C O M M A N D S  S T U F F
 //
 
 bot.onText(/^\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'This is a bot based on NodeJS and Wallhaven\'s API!\
-    \nType /help to get a list of available commands.',
-        {
-            reply_to_message_id: msg.message_id,
-        });
+    if (isAuthorized(msg) < 0) {
+        sendUnauthorizedMessage(msg);
+    } else {
+        bot.sendMessage(msg.chat.id, `This is a bot based on NodeJS and Wallhaven\'s API!\
+        \nType /help to get a list of available commands.`,
+            {
+                reply_to_message_id: msg.message_id,
+            });
+    }
 });
 
 bot.onText(/^\/ping/, (msg) => {
-    ping(hosts).then(function (delta) {
-        bot.sendMessage(msg.chat.id, 'Ping time was ' + String(delta) + ' ms.', {
-            reply_to_message_id: msg.message_id,
+    if (isAuthorized(msg) < 0) {
+        sendUnauthorizedMessage(msg);
+    } else {
+        ping(hosts).then(function (delta) {
+            bot.sendMessage(msg.chat.id, `Ping time was ` + String(delta) + ` ms.`, {
+                reply_to_message_id: msg.message_id,
+            });
+            console.log(`Starting ping test. Ping time was ` + String(delta) + ` ms`);
+        }).catch(function (err) {
+            console.error(`Could not ping remote URL`, err);
         });
-        console.log('Starting ping test. Ping time was ' + String(delta) + ' ms');
-    }).catch(function (err) {
-        console.error('Could not ping remote URL', err);
-    });
+    }
 });
 
 bot.onText(/\/search (.+)/, async (msg, match) => {
-    const resp = await search(match[1]);
-    resp != "404"
-        ? bot.sendDocument(msg.chat.id, resp, {
-            reply_to_message_id: msg.message_id,
-        })
-        : bot.sendMessage(msg.chat.id, "Try again with some other keyword(s)", {
-            reply_to_message_id: msg.message_id,
-        });
+    if (isAuthorized(msg) < 0) {
+        sendUnauthorizedMessage(msg);
+    } else {
+        const resp = await search(match[1]);
+        resp != `404`
+            ? bot.sendDocument(msg.chat.id, resp, {
+                reply_to_message_id: msg.message_id,
+            })
+            : bot.sendMessage(msg.chat.id, `Try again with some other keyword(s)`, {
+                reply_to_message_id: msg.message_id,
+            });
+    }
 });
 
 bot.onText(/\/random/, async (msg) => {
-    const resp = await random();
-    bot.sendDocument(msg.chat.id, resp, {
-        reply_to_message_id: msg.message_id,
-    });
-});
-
-bot.onText(/\/nsfw/, async (msg) => {
-    const resp = await nsfw();
-    bot.sendDocument(msg.chat.id, resp, {
-        reply_to_message_id: msg.message_id,
-    });
-});
-
-bot.onText(/\/getwall (.+)/, async (msg, match) => {
-    const resp = await get_wall_using_id(match[1]);
-    resp != "404"
-        ? bot.sendDocument(msg.chat.id, resp, {
-            reply_to_message_id: msg.message_id,
-        })
-        : bot.sendMessage(msg.chat.id, "Try using another wallpaper ID", {
-            reply_to_message_id: msg.message_id,
-        });
-});
-
-bot.onText(/\/top (.+)/, async (msg, match) => {
-    const resp = await toplist(match[1]);
-    for (let i = 0; i < 5; i++) {
-        bot.sendDocument(msg.chat.id, resp[i], {
+    if (isAuthorized(msg) < 0) {
+        sendUnauthorizedMessage(msg);
+    } else {
+        const resp = await random();
+        bot.sendDocument(msg.chat.id, resp, {
             reply_to_message_id: msg.message_id,
         });
     }
 });
 
+bot.onText(/\/nsfw/, async (msg) => {
+    if (isAuthorized(msg) < 0) {
+        sendUnauthorizedMessage(msg);
+    } else {
+        const resp = await nsfw();
+        bot.sendDocument(msg.chat.id, resp, {
+            reply_to_message_id: msg.message_id,
+        });
+    }
+});
+
+bot.onText(/\/getwall (.+)/, async (msg, match) => {
+    if (isAuthorized(msg) < 0) {
+        sendUnauthorizedMessage(msg);
+    } else {
+        const resp = await get_wall_using_id(match[1]);
+        resp != `404`
+            ? bot.sendDocument(msg.chat.id, resp, {
+                reply_to_message_id: msg.message_id,
+            })
+            : bot.sendMessage(msg.chat.id, `Try using another wallpaper ID`, {
+                reply_to_message_id: msg.message_id,
+            });
+    }
+});
+
+bot.onText(/\/top (.+)/, async (msg, match) => {
+    if (isAuthorized(msg) < 0) {
+        sendUnauthorizedMessage(msg);
+    } else {
+        const resp = await toplist(match[1]);
+        for (let i = 0; i < 5; i++) {
+            bot.sendDocument(msg.chat.id, resp[i], {
+                reply_to_message_id: msg.message_id,
+            });
+            console.log()
+        }
+    }
+});
+
 bot.onText(/\/help/, (msg) => {
-    bot.sendMessage(
-        msg.chat.id, "/help : To get this message \
+    if (isAuthorized(msg) < 0) {
+        sendUnauthorizedMessage(msg);
+    } else {
+        bot.sendMessage(
+            msg.chat.id, `/help : To get this message \
         \n\n/search <keyword> : To get a wallpaper related to the keyword \
         \n\n/nsfw : To get a random NSFW image from Wallhaven \
         \n\n/random : To get any random image from Wallhaven \
         \n\n/ping : To test the ping of the bot with telegram/google \
         \n\n/getwall <id> : Download the wallpaper using it's id \
-        \n\n/top <1d/3d/1w/1M/3M/6M/1y> : Returns 5 images based on the specified toprange",
-        {
-            reply_to_message_id: msg.message_id,
-        })
+        \n\n/top <1d/3d/1w/1M/3M/6M/1y> : Returns 5 images based on the specified toprange`,
+            {
+                reply_to_message_id: msg.message_id,
+            });
+    }
 });
+
+//
+//  F U N C T I O N S
+//
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function isAuthorized(msg) {
+    for (var i = 0; i < config.AUTH_USERS.length; i++) {
+        if (config.AUTH_USERS[i] == msg.from.id) return 0;
+    }
+    if (config.AUTH_CHATS.indexOf(msg.chat.id) > -1 &&
+        msg.chat.all_members_are_administrators) return 2;
+    if (config.AUTH_CHATS.indexOf(msg.chat.id) > -1) return 3;
+    return -1;
+}
+
+function sendUnauthorizedMessage(msg) {
+    bot.sendMessage(msg.chat.id, `You don't have the permission to use this bot.`, {
+        reply_to_message_id: msg.message_id,
+    });
+}
--- example_config.js
+++ example_config.js
@@ -1,5 +1,6 @@
 module.exports = Object.freeze({
   TOKEN: 'bot_token',
   API_WALLHAVEN: 'wallhaven_api',
-  OWNER_ID: [],
+  AUTH_USERS: [123, 456],
+  AUTH_CHATS: [123, 456],
 });
