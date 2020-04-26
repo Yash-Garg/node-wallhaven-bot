6c85056146e02a7971795c583b973c2a89b02601
Yash Garg <ben10.yashgarg@gmail.com>
2020-04-27 03:56:29 +0530

    Use purity = 110 in random and search function

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ const search = async (query) => {
     let args = query.split(" ");
     let q = "search?q=" + args.join("+");
     let response = await axios.get(
-        `${API_URL}${q}&sorting=random${API_INDEX}`
+        `${API_URL}${q}&sorting=random&purity=110${API_INDEX}`
     );
     let path = await response.data.data[1].path;
     let image = await response.data.data[1].thumbs.small;
@@ -32,7 +32,7 @@ const search = async (query) => {
 
 const random = async () => {
     let response = await axios.get(
-        `${API_URL}search?sorting=random${API_INDEX}`
+        `${API_URL}search?sorting=random&purity=110${API_INDEX}`
     );
     let path = await response.data.data[1].path;
     let image = await response.data.data[1].thumbs.small;
